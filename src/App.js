90142bc17dcd99d53bfe7c68337584f1f4bf823c
irfanjsk <119561154+irfanjsk@users.noreply.github.com>
2023-07-29 10:52:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Expenses from "./components/Expenses";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Expenses />
+    </div>
+  );
+}
