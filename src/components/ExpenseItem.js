1c1bbb7bdcd9475ec9120c3e42b284f92f8508ab
Irfan Jamil <119561154+irfanjsk@users.noreply.github.com>
2023-07-31 00:57:27 +0000

    Update 2 files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,15 +2,26 @@ import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
+import { useState } from "react/cjs/react.production.min";
 
 function ExpenseItem(props) {
+  const [price, setPrice] = useState(100);
+  const handlePriceClick = () => {
+    setPrice(75);
+  };
+
+  const handleClick = () => {
+    console.log("Clicked !!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2> {props.title} </h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
-      <div className="expense-item__price">{props.amount}</div>
+      <button onClick={handleClick}>Change Title </button>
     </Card>
   );
 }
