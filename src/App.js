164fbb001743b791e21180616ae639344a51632d
Irfan Jamil <119561154+irfanjsk@users.noreply.github.com>
2023-07-31 00:58:51 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <NewExpense />
       <Expenses />
     </div>
   );
