0f9da77bf76a8d9e18798c554ce391a8bf847b54
Irfan Jamil <119561154+irfanjsk@users.noreply.github.com>
2023-08-05 10:28:24 +0000

    Update 4 files

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,16 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses() {
+  const [filteredYear, setfilteredYear] = useState("2022");
+  const filterChnageHandler = (selectedYear) => {
+    console.log("Exepnses.js");
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
   const expenses = [
     {
       id: "e1",
@@ -25,28 +33,34 @@ function Expenses() {
     },
   ];
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          addExepnsesYear={filterChnageHandler}
+        />
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
